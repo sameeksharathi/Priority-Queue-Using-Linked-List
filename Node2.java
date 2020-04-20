3df391305f47655c8c0a6b4551f7d5d65d4f18cf
Sameeksha Rathi <43092943+sameeksharathi@users.noreply.github.com>
2020-04-20 17:39:10 +0530

    Created a node class

---
+++ Node2.java
@@ -0,0 +1,10 @@
+class Node2{
+    int data;
+    Node2 next;
+    int priority;
+    Node2(int data,int priority){
+        this.data = data;
+        this.priority = priority;
+        next = null;
+    }
+}
